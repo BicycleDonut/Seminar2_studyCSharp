ad5c76db887c53f0f354a705a50704560620407d
Alexander Ryzhov <aryzhov84@gmail.com>
2022-08-08 19:10:01 +0300

    add T9

---
+++ T9/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая случайное число из отрезка [10,99]
+// и показывает наибольшую цифру числа.
+//78 8
+//12 2
+//85 8
+
+int number= new Random().Next(1,100);
+Console.WriteLine($"Наше случайное число: {number}");
+int digit1=number/10;
+int digit2=number%10;
+if(digit1>digit2)
+{
+    Console.WriteLine(digit1);
+}
+    else
+{
+    Console.WriteLine(digit2); 
+}
